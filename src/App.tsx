b4da1cfa629e19bb4e4b6963cd1b9e2937fd54c9
masud001 <madhnagar@gmail.com>
2023-12-31 04:47:56 +0600

    editable content experiment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 /** @format */
+import { useState } from 'react';
+import ContentEditable from 'react-contenteditable';
 
-function App() {
+interface ComponentState {
+	isEditable: boolean;
+	htmlContent: string;
+}
+
+const App: React.FC = () => {
+	const [state, setState] = useState<ComponentState>({
+		isEditable: true,
+		htmlContent: '<h1>this is html <b>content</b> </h1>',
+	});
+	const handleChange = (event: any) => {
+		// setState(
+		// 	state.htmlContent: event.target.value
+		// );
+	};
 	return (
 		<>
 			<h1 className='text-3xl font-bold underline text-cyan-500'>Hello resume builder.</h1>
+			<ContentEditable
+				className='content_editable'
+				tagName='pre'
+				html={state.htmlContent}
+				onChange={handleChange}
+				onBlur={() => console.log('this is the on blur')}
+			/>
 		</>
 	);
-}
+};
 
 export default App;
